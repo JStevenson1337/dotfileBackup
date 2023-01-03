53c56a3aa4dc777e8ace111ab5406b7be28b67aa
Jeremy Stevenson <jstevenson918@gmail.com>
2023-01-03 01:55:28 -0600

    Began script add list of packages to install

--- backup.sh
+++ backup.sh
@@ -1,3 +1,37 @@
 #!/usr/bin/env bash
 
+set -eu -o pipefail # Fail on error and report it, debug all lines
+
+sudo -n true
+test $? -eq 0 || exit 1 "You should have sudo privilages to run this script!"
+
+echo Installing Updates!...
+
+cat packages.list | while read line
+do
+    echo $line
+    wait 
+done
+
+
+# Update 
+
+
+#.zshrc
+#.config/
+
+
+#ssh-keygen -t ed22519 -C 'jmstevenson@gmail.com'
+
+#sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+ #      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+#.local/share/nvim
+
+#cd
+#git clone https://github.com/gpakosz/.tmux.git
+#ln -s -f .tmux/.tmux.conf
+#cp .tmux/.tmux.conf.local .
 
